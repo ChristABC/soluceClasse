d48acab0709cd7cc62231a7a5dc16e05ea4a04c0
Christ ASSANA <christassana@gmail.com>
2024-06-19 17:30:03 +0200

    Ajoutez des fichiers projet.

---
+++ projClasse/Character.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projCharacter
+{
+    public class Character
+    {
+        public string nameCharacter;
+        public float lifePoints;
+        public float lifeDef;
+        public float lifeAttak;
+        public Character()
+        {
+            nameCharacter = "Toto";
+            lifeAttak = 50;
+            lifeDef = 20;
+            lifePoints = 70;
+
+        }
+
+        public Character(string _nameCharacter, string _lifeDef, string _lifeAttak, string _lifePoints)
+        {
+            nameCharacter = _nameCharacter;
+            lifeDef = float.Parse(_lifeDef);
+            lifeAttak = float.Parse(_lifeAttak);
+            lifePoints = float.Parse(_lifePoints);
+        }
+        public void Attaque( Character j2)
+        {
+            if (this.lifeAttak > j2.lifeDef)
+            {
+                Console.WriteLine("Le " + nameCharacter + " attaque le " + j2.nameCharacter);
+                j2.lifePoints = j2.lifePoints - (lifeAttak - j2.lifeDef);
+                Console.WriteLine("Le joureur " + nameCharacter + " a pour nombre de points de vie : " + lifePoints);
+            }
+            else
+            {
+                Console.WriteLine("Rien ne se passe");
+            }
+        }
+
+        public void Combat(Character j2)
+        {
+            bool booleen = false;
+            
+            while (lifePoints > 0 && j2.lifePoints > 0)
+            {
+                if (booleen == false)
+                {
+                    Attaque(j2);
+                    booleen = true;
+
+                }
+                else
+                {
+                    j2.Attaque(this);
+                    booleen = false;
+
+                }
+            }
+            if (this.lifePoints < 0 && j2.lifePoints >= 0)
+            {
+                Console.WriteLine("Le joueur " + j2.nameCharacter + "qui a gagné avec ce score: " + j2.lifePoints);
+
+            }
+            else if (j2.lifePoints < 0 && this.lifePoints >= 0)
+            {
+                Console.WriteLine("Le joueur " + this.nameCharacter + " a gagné avec ce score : " + this.lifePoints);
+
+            }
+            else
+            {
+                Console.WriteLine("Vous avez fini égalité");
+
+            }
+        }
+
+    }
+}
---
+++ projClasse/Program.cs
@@ -0,0 +1,21 @@
+﻿﻿using System;
+using System.ComponentModel.Design;
+using System.Reflection.Metadata.Ecma335;
+using projCharacter;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+
+class MainProgram
+{
+    public static void Main(string[] args)
+    {
+        Character j1 = new Character("Paul", "20", "40", "50");
+        Character j2 = new Character();
+
+        j1.Combat(j2);
+
+        }
+
+    
+    
+ }
